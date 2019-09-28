be704a5ec290c15d1b06cdb896d0305a0f0cf940
Peter <sppaulsson@gmail.com>
2019-09-28 13:26:12 +0200

    First git

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Page Title</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/base.css">
+</head>
+
+<body>
+
+
+<div class="fullimg">
+
+	
+<div class="head">
+
+<div style="padding:40px; background-color: white; opacity: 0.6; cursor: pointer;" onclick="openNav(info)">Thai In </div>
+<div> 
+	<div class="meny" onclick="openNav(mymeny)">Meny</div>
+
+</div>
+
+</div>
+
+
+<div id="info" class="info">
+	<div>Telefon: <span style="font-weight: 600;">0431 - 107 60</span></div>
+	<div style="font-weight: 600;">Höjavägen 13, Ängelholm</div><br>
+	<div>Öppet</div>
+	<div style="font-weight: 600;">Mån-fre: 11:30 - 14:00, 16:00 - 21:00</div>
+	<div style="font-weight: 600;">Lör: 16:00 - 21:00</div>
+	<div style="font-weight: 600;">Sön: Stängt</div>
+</div>
+
+<div id="mymeny" class="info mymeny">
+	<div style="font-size: 1.2rem;">Meny.</div><br>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 1 Phad med Ma Måomg.</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 2 Phadkrapao Välj styrka:  Lite-, Mellan-, Mycket Stark</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 2 Phadkrapao Välj styrka:  Lite-, Mellan-, Mycket Stark</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 2 Phadkrapao Välj styrka:  Lite-, Mellan-, Mycket Stark</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 2 Phadkrapao Välj styrka:  Lite-, Mellan-, Mycket Stark</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+
+	<div class="detalj">
+		<div style="font-weight: 600;">nr. 2 Phadkrapao Välj styrka:  Lite-, Mellan-, Mycket Stark</div>
+		<div>Wokade grönsaker med cashewnötter (med ris)</div>
+		<div>Kyckling 79:- , Biff 85:- , Fläskkött 85:- , Räkor 89:- , Anka 89:- </div>
+
+	</div>
+</div>
+
+
+
+
+
+</div>
+
+
+
+
+<script src="js/myjs.js"></script>
+</body>
+</html>
---
+++ js/myjs.js
@@ -0,0 +1,24 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function openNav(a){
+	
+	if (a.style.display === 'block'){
+  		a.style.display = 'none';
+  	}else{
+  		a.style.display = 'block';
+  	}
+ 
+}
